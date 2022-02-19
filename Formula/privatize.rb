53c7c25de20bbe2d833604325aa1cc187e6161f4
Justin Higgins <justin@turntable.fm>
2022-02-19 12:56:46 -0800

    release 0.1.1

--- Formula/privatize.rb
+++ Formula/privatize.rb
@@ -3,10 +3,10 @@
 class Privatize < Formula
   desc "Partially encrypt/decrypt HEREDOCS. Automatically in git or standalone"
   homepage "https://github.com/higgins/privatize"
-  url "https://github.com/higgins/privatize/releases/download/0.1.0/privatize.tar.gz"
-  sha256 "3c25b0156da32cc7cc6688fbdbf0c82994ee9f72968dea5398f2b5316c7f3ff3"
+  url "https://github.com/higgins/privatize/releases/download/0.1.1/privatize.tar.gz"
+  sha256 "065442b26b77d63e9898f2887db73698196a3fa358146b5b099504fe57007b27"
   license "ISC"
-  version "0.1.0"
+  version "0.1.1"
 
   def install
     bin.install "privatize"
